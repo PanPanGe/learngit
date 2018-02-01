93372709519c331b2161f94e3393c849dd3bd44e
PanPanGe <36029561+PanPanGe@users.noreply.github.com>
2018-02-01 16:44:14 +0800

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+d = {'Micheal': 95, 'Bob': 85, 'Tracy': 78}
+print('d[\'Miacheal\'] =',d['Micheal'])
+print('d[\'Bob\'] = ',d['Bob'])
+print('d.get(\'Adam\', -1) = ',d.get('Adam',-1))
